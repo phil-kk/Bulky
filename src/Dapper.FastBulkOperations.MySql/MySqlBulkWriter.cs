8954b153e042331d50821839c5ab6081201bf0c8
filipp <filipp.kleymyonov@gmail.com>
2022-11-19 11:51:08 +0300

    SET GLOBAL local_infile = true

--- src/Dapper.FastBulkOperations.MySql/MySqlBulkWriter.cs
+++ src/Dapper.FastBulkOperations.MySql/MySqlBulkWriter.cs
@@ -16,10 +16,25 @@ public sealed class MySqlBulkWriter : IBulkWriter
     {
         _dialect = dialect;
     }
+
+    private void SetLoadInFile(MySqlConnection connection)
+    {
+        using var cmd = connection.CreateCommand();
+        cmd.CommandText = "SET GLOBAL local_infile = true;";
+        cmd.ExecuteNonQuery();
+    }
+    
+    private async Task SetLoadInFileAsync(MySqlConnection connection)
+    {
+        await using var cmd = connection.CreateCommand();
+        cmd.CommandText = "SET GLOBAL local_infile = true;";
+        await cmd.ExecuteNonQueryAsync();
+    }
     public void Write<T>(DbConnection connection, DbTransaction transaction, int timeout, int batchSize, IEnumerable<T> items,
         IEnumerable<KeyValuePair<string, Member>> mapping, string tableName)
     {
         if (connection is not MySqlConnection mySqlConnection) return;
+        SetLoadInFile(mySqlConnection);
         var ordered = mapping.OrderBy(x => x.Key).ToArray();
         var objectReader = items.ToObjectDapperReader(_dialect, ordered.Select(x => x.Value.Name).ToArray());
         var bulkCopy = new MySqlBulkCopy(mySqlConnection, transaction as MySqlTransaction);
@@ -38,6 +53,7 @@ public sealed class MySqlBulkWriter : IBulkWriter
         IEnumerable<KeyValuePair<string, Member>> mapping, string tableName)
     {
         if (connection is not MySqlConnection mySqlConnection) return;
+        await SetLoadInFileAsync(mySqlConnection);
         var ordered = mapping.OrderBy(x => x.Key).ToArray();
         var objectReader = items.ToObjectDapperReader(_dialect, ordered.Select(x => x.Value.Name).ToArray());
         var bulkCopy = new MySqlBulkCopy(mySqlConnection, transaction as MySqlTransaction);
